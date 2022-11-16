6019a827c69b14ed321f5f5119d2485072b25377
notpikmin <notpikmin@gmail.com>
2022-11-16 14:39:33 -0800

    init

---
+++ YoutubeDownloader/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace YoutubeDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Download(object sender, RoutedEventArgs e)
+        {
+            string url = VideoUrl.Text;
+            Process process = new Process();
+            var args = "-x --audio-format mp3 " + url + " -o '%(title)s.%(ext)s'";
+            process.StartInfo = new ProcessStartInfo("yt-dlp.exe", args);
+            process.Start();
+        }
+    }
+}
